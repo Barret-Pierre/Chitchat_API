5901585750ef442f3f3eda3b97a281007d3c78fc
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2023-12-11 07:36:54 +0100

    Ops : configure env file or déployement

--- database/chitchat_db.sql
+++ database/chitchat_db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `test`;
+CREATE DATABASE  IF NOT EXISTS `chichat_api` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `chichat_api`;
 -- MySQL dump 10.13  Distrib 8.0.33, for macos13 (arm64)
 --
 -- Host: 127.0.0.1    Database: chitchat_api
--- database/config.js
+++ database/config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const mysqlPool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "chitchat-admin",
+  password: process.env.DB_PASSWORD || "Azerty123456?",
+  database: process.env.DB_DATABASE || "chitchat_api",
 });
 
 module.exports = mysqlPool;
